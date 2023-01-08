77d7299e3fd8ff8cd249ce1346611cb156ec4c2f
jssdestroyerman <114258377+jssdestroyerman@users.noreply.github.com>
2023-01-08 22:12:35 +0100

    error page done

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Error() {
     return (
@@ -10,6 +11,7 @@ function Error() {
                 <h2>Oups! La page que vous demandez n'existe pas.</h2>
                 <NavLink to={"./"}>Retourner sur la page d'accueil</NavLink>
             </div>
+            <Footer />
         </>
     );
 }
