81dcf878df474c78a472f7333e30e4c302f87cd0
jssdestroyerman <114258377+jssdestroyerman@users.noreply.github.com>
2023-01-09 16:06:07 +0100

    working on house part

--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,23 +1,24 @@
 import database from "../database.json";
 import Header from "../components/Header";
+import { useState } from "react";
+import arrow from "../assets/arrow.svg";
 
-function Logement() {
+function Housing() {
     let url = new URL(window.location.href);
     let pageId = url.searchParams.get("id");
 
     const data = database.find((element) => element.id === pageId);
     console.log(data);
-
-    // try use Effect
+    const [image, setImage] = useState(data.pictures[0]);
 
     return (
         <>
             <Header />
             <div className="housingBanner">
-                <img src={} alt="" />
+                <img src={image} alt="" />
             </div>
         </>
     );
 }
 
-export default Logement;
+export default Housing;
