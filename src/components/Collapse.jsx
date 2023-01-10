a6811de9532d3c48946686635648d76b65a03c19
jssdestroyerman <114258377+jssdestroyerman@users.noreply.github.com>
2023-01-10 22:26:37 +0100

    collapse arrow updated

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import arrow from "../assets/arrow.svg";
 
 function AboutList({ name, content }) {
     const [isHidden, setIsHidden] = useState(true);
@@ -13,11 +12,19 @@ function AboutList({ name, content }) {
                     }}
                 >
                     {name}
-                    <img
+                    <svg
                         className={!isHidden ? "arrowOpen" : ""}
-                        src={arrow}
-                        alt="Arrow"
-                    />
+                        width="25"
+                        height="15"
+                        viewBox="0 0 25 15"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M2.6635 0.859489L0.530579 3.00462L12.4605 14.9233L24.3904 2.99257L22.2575 0.859489L12.4605 10.6572L2.6635 0.859489Z"
+                            fill="white"
+                        />
+                    </svg>
                 </h2>
                 <p className={!isHidden ? "contentOpen" : ""}>{content}</p>
             </div>
