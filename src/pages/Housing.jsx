efb7c905cb43e1ff9609955ed42f0c084452924e
jssdestroyerman <114258377+jssdestroyerman@users.noreply.github.com>
2023-01-13 10:47:51 +0100

    housing page working

--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -3,64 +3,70 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Collapse from "../components/Collapse";
 import Star from "../components/Star";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import Carrousel from "../components/Carrousel";
-import Error from "../pages/Error";
 
 function Housing() {
     const { id } = useParams();
     const data = database.find((element) => element.id === id);
 
-    if (data) {
-        return (
-            <>
-                <Header />
-                <Carrousel data={data} />
+    return (
+        <>
+            {!data ? (
+                <Navigate to="*" replace={true} />
+            ) : (
+                <>
+                    <Header />
+                    <Carrousel data={data} />
 
-                <div className="informations">
-                    <div className="left">
-                        <h1>{data.title}</h1>
-                        <p>{data.location}</p>
-                        <ul>
-                            {data.tags.map((tag, index) => (
-                                <li key={index}>{tag}</li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="right">
-                        <div className="hostInformations">
-                            <p>{data.host.name}</p>
-                            <img src={data.host.picture} alt="propriétaire" />
+                    <div className="informations">
+                        <div className="left">
+                            <h1>{data.title}</h1>
+                            <p>{data.location}</p>
+                            <ul>
+                                {data.tags.map((tag, index) => (
+                                    <li key={index}>{tag}</li>
+                                ))}
+                            </ul>
                         </div>
-                        <div className="rating">
-                            <Star rating={data.rating} />
+                        <div className="right">
+                            <div className="hostInformations">
+                                <p>{data.host.name}</p>
+                                <img
+                                    src={data.host.picture}
+                                    alt="propriétaire"
+                                />
+                            </div>
+                            <div className="rating">
+                                <Star rating={data.rating} />
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="housingCollapse">
-                    <div className="collapse">
-                        <Collapse
-                            name={"Description"}
-                            content={data.description}
-                        />
+                    <div className="housingCollapse">
+                        <div className="collapse">
+                            <Collapse
+                                name={"Description"}
+                                content={data.description}
+                            />
+                        </div>
+                        <div className="collapse">
+                            <Collapse
+                                name={"Équipements"}
+                                content={data.equipments.map(
+                                    (equipement, index) => {
+                                        return (
+                                            <li key={index}>{equipement}</li>
+                                        );
+                                    }
+                                )}
+                            />
+                        </div>
                     </div>
-                    <div className="collapse">
-                        <Collapse
-                            name={"Équipements"}
-                            content={data.equipments.map(
-                                (equipement, index) => {
-                                    return <li key={index}>{equipement}</li>;
-                                }
-                            )}
-                        />
-                    </div>
-                </div>
-                <Footer />
-            </>
-        );
-    } else {
-        return <Error />;
-    }
+                    <Footer />
+                </>
+            )}
+        </>
+    );
 }
 
 export default Housing;
