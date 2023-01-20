07fb330fb50520383a29dd2d49b848431ac0b91e
jssdestroyerman <jesuisdestroyerman@proton.me>
2023-01-20 11:37:49 +0100

    pagination done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,22 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import database from "../database.json";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function Home() {
+    const [data, setData] = useState(database);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage, setPostsPerPage] = useState(6);
+    const pages = [];
+
+    const lastPostIndex = currentPage * postsPerPage;
+    const firstPostIndex = lastPostIndex - postsPerPage;
+    const currentPosts = data.slice(firstPostIndex, lastPostIndex);
+
+    for (let i = 1; i <= Math.ceil(data.length / postsPerPage); i++) {
+        pages.push(i);
+    }
+
     return (
         <>
             <Header />
@@ -11,7 +25,7 @@ function Home() {
                 <p>Chez vous, partout et ailleurs</p>
             </div>
             <div className="card-container">
-                {database.map((logement) => {
+                {currentPosts.map((logement) => {
                     return (
                         <NavLink
                             key={logement.id}
@@ -21,6 +35,19 @@ function Home() {
                             <img src={logement.pictures[0]} alt="" />
                             <p>{logement.title}</p>
                         </NavLink>
+                    );
+                })}
+            </div>
+            <div className="pagination">
+                {pages.map((page, index) => {
+                    return (
+                        <button
+                            onClick={() => setCurrentPage(page)}
+                            key={index}
+                            className={page === currentPage ? "isActive" : ""}
+                        >
+                            {page}
+                        </button>
                     );
                 })}
             </div>
