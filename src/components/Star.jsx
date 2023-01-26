9c14bdfd1a9a58ce87ccfc04509e0eea0b0ca837
jssdestroyerman <jesuisdestroyerman@proton.me>
2023-01-26 15:20:33 +0100

    rework of stars

--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -1,12 +1,37 @@
 function Star({ rating }) {
     const stars = [1, 2, 3, 4, 5];
+    const abc = 5 - rating;
+    let emptyStars = [];
+
+    for (let i = 1; i <= abc; i++) {
+        emptyStars.push(i);
+    }
 
     return (
         <>
-            <div className="emptyStars">
+            <div className="filledStars">
                 {stars.map((star, index) => {
                     return (
-                        star <= 5 && (
+                        star <= rating && (
+                            <svg
+                                key={index}
+                                width="30"
+                                height="30"
+                                viewBox="0 0 30 30"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z"
+                                    fill="#ff6060"
+                                />
+                            </svg>
+                        )
+                    );
+                })}
+                {emptyStars.map((star, index) => {
+                    return (
+                        star && (
                             <svg
                                 key={index}
                                 width="30"
@@ -23,27 +48,6 @@ function Star({ rating }) {
                         )
                     );
                 })}
-                <div className="filledStars">
-                    {stars.map((star, index) => {
-                        return (
-                            star <= rating && (
-                                <svg
-                                    key={index}
-                                    width="30"
-                                    height="30"
-                                    viewBox="0 0 30 30"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z"
-                                        fill="#ff6060"
-                                    />
-                                </svg>
-                            )
-                        );
-                    })}
-                </div>
             </div>
         </>
     );
